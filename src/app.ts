456645d11c8759a16dc0ad14c98fd961a29b73a2
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-08 01:50:22 -0400

    Criando rota Github com Authentication ID

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express()
 
+app.get('/github', (req, res) => {
+    res.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
 app.listen(4000, () => console.log('Server is Running PORT 4000'))
