cc2e6d7c81cb19d8dfda1bcec3e92c4c4ac4d91c
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-08 01:56:36 -0400

    Criando rota de Callback para retornar ID de acesso do User

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import "dotenv/config"
+import express, { json } from "express";
 
 const app = express()
 
 app.get('/github', (req, res) => {
     res.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (req, res) => {
+    const { code } = req.query
+
+    return res.json(code)
 })
 
 app.listen(4000, () => console.log('Server is Running PORT 4000'))
