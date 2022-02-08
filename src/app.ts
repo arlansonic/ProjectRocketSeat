52cdd5c4884114b57972d5078fca5d8c249eeba4
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-08 19:36:05 -0400

    Criando Access Token e conectando via JsonWebToken - Autorization no Github

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { router } from './routes'
 
 const app = express()
+app.use(express.json())
 app.use(router)
 
 app.get('/github', (req, res) => {
@@ -13,6 +14,10 @@ app.get('/signin/callback', (req, res) => {
     const { code } = req.query
 
     return res.json(code)
+})
+
+app.get('/', (req, res) => {
+    res.send('Api is Running on: 4000')
 })
 
 app.listen(4000, () => console.log('Server is Running PORT 4000'))
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 /**
- * Receber o Code(String)
+ * Receber o Code(String) * 
+ * Recuperar informações no GitHub
  * Recuperar o Acces_Token no Github
  * Verificar se o usuario existe no DB
  * ----- SIM = Gera um Token
@@ -9,22 +10,30 @@ import axios from "axios"
  * Retornar o Token com as informações do user logado
  */
 
+interface IAccessTokenResponse {
+    access_token: string
+}
 
 class AuthenticateUserService {
     async execute(code: string) {
         const url = "https://github.com/login/oauth/access_token"
 
-        const res = await axios.post(url, null, {
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
                 code
             },
             headers: {
-                "Accept": "application/json"
+                Accept: "application/json"
             }
         })
 
+        const res = await axios.get("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
+            }
+        })
         return res.data
     }
 }
