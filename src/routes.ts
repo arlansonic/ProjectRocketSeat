a304a0e538dbb48e927aeb9745bf01ca9a597486
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-09 00:47:24 -0400

    Mostrar Usuário conectado: Rota /profile

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { AutenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreteMessageController";
+import { Get3LasMessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router()
@@ -8,5 +10,9 @@ const router = Router()
 router.post('/authenticate', new AutenticateUserController().handle)
 
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
+
+router.get('/messages/last3', new Get3LasMessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
