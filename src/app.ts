4d7cc882108b9d5c911dd23249b9bb5acac9529f
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-09 00:17:04 -0400

    Adicionanado Socket.io para receber as mensagme instantâneas

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import "dotenv/config"
 import express from "express";
 import { router } from './routes'
+import http from 'http'
+import cors from 'cors'
+import { Server, Socket } from "socket.io"
 
 const app = express()
 app.use(express.json())
 app.use(router)
+app.use(cors)
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: '*'
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`Usuário conectado no Socket ${socket.id}`)
+})
 
 app.get('/github', (req, res) => {
     res.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
@@ -20,4 +36,4 @@ app.get('/', (req, res) => {
     res.send('Api is Running on: 4000')
 })
 
-app.listen(4000, () => console.log('Server is Running PORT 4000'))
+export { serverHttp, io }
