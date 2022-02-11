f5dd1c7a1e2437132658407736fb4be897733dd6
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-11 01:01:14 -0400

    Criando a autenticação do Usuário e salvando direto no banco os dados

--- FrontEnd/web/src/components/LoginBox/index.tsx
+++ FrontEnd/web/src/components/LoginBox/index.tsx
@@ -1,15 +1,50 @@
 import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
+import { useEffect } from 'react'
+import { api } from '../../services/api'
+
+type AuthRes = {
+    token: string
+    user: {
+        id: string
+        avatar_url: string
+        name: string
+        login: string
+    }
+}
 
 function LoginBox() {
 
     const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=84ce38dfe481c5543a89`
 
+    async function singIn(githubCode: string) {
+        const res = await api.post<AuthRes>('authenticate', {
+            code: githubCode
+        })
+
+        const { token, user } = res.data
+
+        localStorage.setItem('@dowhile:token', token)
+
+        console.log(user)
+    }
+
+    useEffect(() => {
+        const url = window.location.href
+        const hasGithubCode = url.includes('?code=')
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            window.history.pushState({}, '', urlWithoutCode)
+            singIn(githubCode)
+        }
+    }, [])
     return (
         <div className={styles.LoginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
             <a href={signInUrl} className={styles.singInWithGithub}>
-                <VscGithubInverted size='24'/>
+                <VscGithubInverted size='24' />
                 Entrar com GitHub
             </a>
         </div>
