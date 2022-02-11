8439eb9ccfcce64cad48b86cbe6ac4a325dd5e7b
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-11 00:45:10 -0400

    Autorizando usuario a logar no github - LoginBox

--- FrontEnd/web/src/components/LoginBox/index.tsx
+++ FrontEnd/web/src/components/LoginBox/index.tsx
@@ -2,10 +2,13 @@ import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
 
 function LoginBox() {
+
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=84ce38dfe481c5543a89`
+
     return (
         <div className={styles.LoginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="$" className={styles.singInWithGithub}>
+            <a href={signInUrl} className={styles.singInWithGithub}>
                 <VscGithubInverted size='24'/>
                 Entrar com GitHub
             </a>
