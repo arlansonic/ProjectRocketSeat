ee3c9e264edbd0e83cca38e15d5f5122280caf78
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-10 22:22:28 -0400

    Atualizando Style LoginBox e MessageList

--- FrontEnd/web/src/components/LoginBox/index.tsx
+++ FrontEnd/web/src/components/LoginBox/index.tsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.scss'
+import { VscGithubInverted } from 'react-icons/vsc'
 
 function LoginBox() {
     return (
         <div className={styles.LoginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
             <a href="$" className={styles.singInWithGithub}>
+                <VscGithubInverted size='24'/>
                 Entrar com GitHub
             </a>
         </div>
