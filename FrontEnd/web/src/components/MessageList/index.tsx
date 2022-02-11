b237d0933ef2e90989c02e51fd3984fd879d571a
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-10 22:53:39 -0400

    Conectando a API e mostrando as 3 mensagens recentes no FrontEnd

--- FrontEnd/web/src/components/MessageList/index.tsx
+++ FrontEnd/web/src/components/MessageList/index.tsx
@@ -1,13 +1,23 @@
 import styles from './styles.module.scss'
 import { api } from '../../services/api'
 import logoImg from '../../assets/logo.svg'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+type Message = {
+    id: string
+    text: string
+    user: {
+        name: string
+        avatar_url: string
+    }
+}
 
 function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
     useEffect(() => {
         // Chamada para API
         api.get('messages/last3').then(res => {
-            console.log(res.data)
+            setMessages(res.data)
         })
     }, [])
 
@@ -16,37 +26,20 @@ function MessageList() {
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/arlansonic.png" alt="Arlan Marreiro" />
-                        </div>
-                        <span>Arlan Marreiro</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/arlansonic.png" alt="Arlan Marreiro" />
-                        </div>
-                        <span>Arlan Marreiro</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/arlansonic.png" alt="Arlan Marreiro" />
-                        </div>
-                        <span>Arlan Marreiro</span>
-                    </div>
-                </li>
+                {messages.map(messages => {
+                    return (
+                        <li key={messages.id} className={styles.message}>
+                            <p className={styles.messageContent}>{messages.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={messages.user.avatar_url} alt={messages.user.name} />
+                                </div>
+                                <span>{messages.user.name}</span>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
-
         </div>
     )
 }
