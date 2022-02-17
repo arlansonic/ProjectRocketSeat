1f24dd2a048ece918fb57348fc73c13c2c47caf4
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-17 16:56:14 -0400

    Finalizando estilização DowHile 2021

--- FrontEnd/web/src/components/MessageList/index.tsx
+++ FrontEnd/web/src/components/MessageList/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss'
 import { api } from '../../services/api'
 import logoImg from '../../assets/logo.svg'
+import io from 'socket.io-client'
 import { useEffect, useState } from 'react'
 
 type Message = {
@@ -12,8 +13,34 @@ type Message = {
     }
 }
 
+const messagesQueue: Message[] = []
+
+// Recebendo as mensagens instantâneas
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+    messagesQueue.push(newMessage)
+})
+
 function MessageList() {
     const [messages, setMessages] = useState<Message[]>([])
+
+    // Colocando mensagens em Fila
+
+    useEffect(() => {
+        const timer = setInterval(()=>{
+            if(messagesQueue.length > 0){
+                setMessages(prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean))
+
+                messagesQueue.shift()
+            }
+        },3000)
+    }, [])
+
     useEffect(() => {
         // Chamada para API
         api.get('messages/last3').then(res => {
